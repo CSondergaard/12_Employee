c022bfc57f2f1ccb7f18a739240ee12a5fe04171
casp4 <casp4@LAPTOP-O5C9AQD0>
2016-10-06 10:19:19 +0200

    Add project files.

---
+++ 12_Employee/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Employee
+{
+    class Employee
+    {
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Id { get; set; }
+
+        public Employee(string name, string type, int id)
+        {
+            Name = name;
+            Type = type;
+            Id = id;
+        }
+
+    }
+}
---
+++ 12_Employee/EmployeeRepository.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace _12_Employee
+{
+    internal class EmployeeRepository
+    {
+        public int count { get; set; }
+
+        public int Id { get; set; }
+
+        Dictionary<int, Employee> dic = new Dictionary<int, Employee>();
+
+        public Employee CreateEmployee(string name, string type)
+        {
+            count++;
+            nextId();
+            return new Employee(name, type, Id);
+        }
+
+        public void SaveEmployee(Employee em)
+        {
+            if (dic.ContainsKey(em.Id))
+            {
+                dic[em.Id] = em;
+            }
+            else
+            {
+                dic.Add(em.Id, em);
+            }
+        }
+
+        public Employee LoadEmployee(int i)
+        {
+
+            return dic[i];
+        }
+
+        public List<Employee> FindAllEmployees()
+        {
+
+            return dic.Values.ToList();
+        }
+
+        public void nextId()
+        {
+            Id++;
+        }
+
+        public int CountEmployees()
+        {
+            return count;
+        }
+
+        public void Clear()
+        {
+            count = 0;
+        }
+
+
+
+    }
+}
